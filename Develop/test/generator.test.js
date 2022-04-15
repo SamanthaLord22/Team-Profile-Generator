4f88e94490e15fec77be31286975f41134b8ecac
Samantha Lord <samlord14@outlook.com>
2022-04-14 23:20:46 -0700

    added readme

--- Develop/test/generator.test.js
+++ Develop/test/generator.test.js
@@ -1,17 +1,8 @@
 //link generator.js
 const generator = require('../generator')
 
-//When code is runs in terminal then prompted to enter team managers name, employee id, email address, and office number
 
-//Next an option to select to add an engineer or intern will be prompted
+// propmts about engineer's name, employee id, email address, and github username are entered
 
-// When propmts about engineer's name, employee id, email address, and github username are entered
-
-//then it will return to option to select to add an engineer or intern
-
-//When propmts about intern's name, employee id, email address, and github username are entered
-
-//then it will return to option to select to add an engineer or intern
-//as well as option to finsh building team
 
 //When finish building team is selected then the HTML page is generated
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Team-Profile-Generator
+For this assignment I created a Team Profile Generator.
+When I enter node index.js in the terminal, a series of questions to create Team Profile Generator are prompted to the user.
+The questions ask for the name, title, employee id, email address, and github username.
+The user answers are then generated into text in a index.html file that can be viewed in the browser.
+
+Deployed live URL:
+
+
+Link to Video of use:
